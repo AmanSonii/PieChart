174c32f40ce9a30c30fb6178d78c432cf035f5e8
a_soni <a.soni@eicare.org>
2021-02-28 11:37:08 +0530

    heading changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import PieChartDemonstration from './component/PieChartDemonstration';
 function App() {
   return (
     <div className="App">
+     <div style={{backgroundColor:'white', height:'50px'}}>
+       <h2 style={{ fontFamily:'URW Chancery L, cursive', marginLeft:"5px", color:'blue', fontWeight:"bold"}}>Pie Chart</h2>        
+       </div> 
       <PieChartDemonstration></PieChartDemonstration>
     </div>
   );
